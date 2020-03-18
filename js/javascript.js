05e927d081b5ce610d29a70633dcfbe328b1dbc5
chriskurien <40710928+chriskurien@users.noreply.github.com>
2020-03-18 16:07:34 -0400

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,4 @@
 //From: https://codepen.io/CheeseTurtle/pen/jzdgI, I changed the css styling and created a keyframe for the animation
-
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
@@ -59,6 +58,36 @@ window.onload = function() {
 //From: https://codepen.io/CheeseTurtle/pen/jzdgI
 
 
+
+// I created this function by reviewing material from:
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_navbar_hide_scroll
+//https://krasimirtsonev.com/blog/article/Using-media-queries-in-JavaScript-AbsurdJS-edition
+//https://stackoverflow.com/questions/46739960/use-multiple-matchmedia-with-svg-for-responsive-viewbox-resizing
+var prevScrollpos = window.pageYOffset;
+
+var mq = [ // list of window.matchMedia() queries
+    window.matchMedia("(min-width: 786px)"),
+    window.matchMedia("(min-width: 900px)")
+    ]
+
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+
+      if(mq[0].matches){
+        document.getElementById("header").style.top = "-125px";
+      }
+      else if(mq[1].matches){
+        document.getElementById("header").style.top = "-75px";
+      }
+      else{
+        document.getElementById("header").style.top = "-220px";
+      }
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 
 
